b37ca5b97918032f7f91b1ab272659ee1104aeeb
Micah <81500988+MicahDevs@users.noreply.github.com>
2021-09-03 15:37:54 -0400

    Update GlobalStyles.tsx

--- styles/GlobalStyles.tsx
+++ styles/GlobalStyles.tsx
@@ -14,7 +14,7 @@ export default function GlobalStyles() {
           --overlay: rgba(0, 0, 0, 0.85);
           --overlay-light: rgba(0, 0, 0, 0.35);
           --border-black: 1px solid var(--1a1b1f);
-          --border-light: 1px solid #dbdbdb;
+          --border-light: 1px solid #1a1b1f;
 
           /* FONTS */
           --font-a: Helvetica, Arial, sans-serif;
